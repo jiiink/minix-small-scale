ef2d4fa30c84a030a94a09119be6397e8620e57f
김병현 <kimbyeonghyeon@myBook.local>
2025-08-28 17:19:47 +0900

    Gemini Refactored

--- source/glo.h
+++ source/glo.h
@@ -1,31 +1,24 @@
-/* EXTERN should be extern except in table.c */
 #ifdef _TABLE
 #undef EXTERN
 #define EXTERN
 #endif
 
-/* Global variables. */
-EXTERN struct mproc *mp;	/* ptr to 'mproc' slot of current process */
-EXTERN int procs_in_use;	/* how many processes are marked as IN_USE */
-EXTERN char monitor_params[MULTIBOOT_PARAM_BUF_SIZE];
-
-/* Misc.c */
-extern struct utsname uts_val;	/* uname info */
-
-/* The parameters of the call are kept here. */
-EXTERN message m_in;		/* the incoming message itself is kept here. */
-EXTERN int who_p, who_e;	/* caller's proc number, endpoint */
-EXTERN int call_nr;		/* system call number */
-
+extern struct mproc *mp;
+extern int procs_in_use;
+extern char monitor_params[MULTIBOOT_PARAM_BUF_SIZE];
+extern struct utsname uts_val;
+extern message m_in;
+extern int who_p;
+extern int who_e;
+extern int call_nr;
 extern int (* const call_vec[])(void);
-EXTERN sigset_t core_sset;	/* which signals cause core images */
-EXTERN sigset_t ign_sset;	/* which signals are by default ignored */
-EXTERN sigset_t noign_sset;	/* which signals cannot be ignored */
-
-EXTERN u32_t system_hz;		/* System clock frequency. */
-EXTERN int abort_flag;
-
-EXTERN struct machine machine;		/* machine info */
+extern sigset_t core_sset;
+extern sigset_t ign_sset;
+extern sigset_t noign_sset;
+extern u32_t system_hz;
+extern int abort_flag;
+extern struct machine machine;
 #ifdef CONFIG_SMP
-EXTERN int cpu_proc[CONFIG_MAX_CPUS];
+extern int cpu_proc[CONFIG_MAX_CPUS];
 #endif
+
